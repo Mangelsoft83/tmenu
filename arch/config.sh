c78641cca8de892510b2671b552ae15cb8c425dd
Mangelsoft83 <71273809+Mangelsoft83@users.noreply.github.com>
2026-06-07 09:58:45 -0400

    Create config.sh

---
+++ arch/config.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+DISK="/dev/nvme0n1"
+EFI_PART="${DISK}p1"
+ROOT_PART="${DISK}p2"
+
+HOSTNAME="arch-thijs"
+USERNAME="thijs"
+TIMEZONE="Europe/Amsterdam"
+LOCALE="en_US.UTF-8"
+KEYMAP="us"
