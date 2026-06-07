e794e5ae4d4b090f1038a8497a22a140c9d55f58
Mangelsoft83 <71273809+Mangelsoft83@users.noreply.github.com>
2026-06-07 10:30:02 -0400

    Update install.sh
    echo

--- arch/install.sh
+++ arch/install.sh
@@ -6,8 +6,10 @@ source ./config.sh
 read -rp "This will erase $DISK. Type YES: " confirm
 [[ "$confirm" == "YES" ]] || exit 1
 
+echo "timedatectl"
 timedatectl set-ntp true
 
+echo "sgdisk"
 sgdisk --zap-all "$DISK"
 sgdisk -n 1:0:+1G -t 1:ef00 -c 1:EFI "$DISK"
 sgdisk -n 2:0:0   -t 2:8300 -c 2:ROOT "$DISK"
@@ -19,8 +21,10 @@ mount "$ROOT_PART" /mnt
 mkdir -p /mnt/boot
 mount "$EFI_PART" /mnt/boot
 
+echo "pacstrap"
 pacstrap -K /mnt $(grep -vE '^\s*#|^\s*$' packages.txt)
 
+echo "genfstav"
 genfstab -U /mnt >> /mnt/etc/fstab
 
 cp config.sh chroot.sh /mnt/root/
