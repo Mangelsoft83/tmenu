74c9597e748ce42b19487f4dd995e3c25dc13583
Thijs <mangelsoft83@gmail.com>
2026-06-11 20:59:37 +0200

    limine

--- arch/chroot.sh
+++ arch/chroot.sh
@@ -27,22 +27,49 @@ passwd "$USERNAME"
 echo "%wheel ALL=(ALL:ALL) ALL" > /etc/sudoers.d/wheel
 chmod 440 /etc/sudoers.d/wheel
 
-bootctl install
+mkdir -p /boot/EFI/BOOT
 
-cat > /boot/loader/loader.conf <<EOF
-default arch.conf
-timeout 3
-console-mode max
-editor no
-EOF
+cp /usr/share/limine/BOOTX64.EFI /boot/EFI/BOOT/
+cp /usr/share/limine/limine.conf /boot/
 
 ROOT_UUID=$(blkid -s UUID -o value "$ROOT_PART")
 
-cat > /boot/loader/entries/arch.conf <<EOF
-title Arch Linux
-linux /vmlinuz-linux
-initrd /intel-ucode.img
-initrd /amd-ucode.img
-initrd /initramfs-linux.img
-options root=UUID=$ROOT_UUID rw
+mkdir -p /boot/EFI/BOOT
+
+cp /usr/share/limine/BOOTX64.EFI /boot/EFI/BOOT/BOOTX64.EFI
+
+cat > /boot/limine.conf <<EOF
+TIMEOUT=5
+
+:Arch Linux
+    PROTOCOL=linux
+    KERNEL_PATH=boot():/vmlinuz-linux
+    CMDLINE=root=UUID=$ROOT_UUID rw
+    MODULE_PATH=boot():/intel-ucode.img
+    MODULE_PATH=boot():/amd-ucode.img
+    MODULE_PATH=boot():/initramfs-linux.img
 EOF
+
+efibootmgr \
+  --create \
+  --disk "$DISK" \
+  --part 1 \
+  --label "Limine" \
+  --loader '\EFI\BOOT\BOOTX64.EFI'
+# cat > /boot/loader/loader.conf <<EOF
+# default arch.conf
+# timeout 3
+# console-mode max
+# editor no
+# EOF
+#
+# ROOT_UUID=$(blkud -s UUID -o value "$ROOT_PART")
+#
+# cat > /boot/loader/entries/arch.conf <<EOF
+# title Arch Linux
+# linux /vmlinuz-linux
+# initrd /intel-ucode.img
+# initrd /amd-ucode.img
+# initrd /initramfs-linux.img
+# options root=UUID=$ROOT_UUID rw
+# EOF
